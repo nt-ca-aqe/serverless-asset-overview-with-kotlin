5e76f753f070490a3e28da2aa197f23057e1f6ad
Pabst Dominik <dominik.pabst@novatec-gmbh.de>
2018-03-23 12:08:01 +0100

    change was lambda function name

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 aws lambda update-function-code \
 --zip-file=fileb://build/distributions/serverless-asset-overview-minimal-1.0-SNAPSHOT.zip \
 --region=eu-central-1 \
---function-name=serverless-asset-overview-minimal
+--function-name=ServerlessAssetOverview_minimal
